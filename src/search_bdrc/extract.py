4f67b9db78ba305820994c95f6aaf671e942f353
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-06-20 09:20:10 +0000

    correct extract code

--- src/search_bdrc/extract.py
+++ src/search_bdrc/extract.py
@@ -1,45 +1,33 @@
 import json
+import re
 from pathlib import Path
 
-import requests
-from rdflib import Graph
 
-
-def write_json(data: dict, output_path: str | Path):
+def write_json(data: dict | list, output_path: str | Path):
     with open(output_path, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
 
-def extract_workhasinstance(g) -> list[str]:
-    works = []
+def extract_instances_id(text: str) -> list[str]:
+    regex = r"<a\shref=\"/show/bdr:([A-Z0-9_]+)\?"
 
-    for subj, pred, obj in g:
-        if str(pred) == "http://purl.bdrc.io/ontology/core/workHasInstance":
-            work_link = str(obj)
-            work = work_link.split("/")[-1]
-            works.append(work)
+    ids = re.findall(regex, text)
 
-    return works
-
-
-def get_work_metadata(work_id: str):
-    url = f"https://purl.bdrc.io/resource/{work_id}.ttl"  # noqa
-    headers = {"Accept": "text/turtle"}  # Requesting Turtle format
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.text
-        g = Graph()
-        g.parse(data=data, format="turtle")
-
-        works = extract_workhasinstance(g)
-        return works
-    else:
-        print(f"Failed to retrieve data: {response.status_code}")
+    # Remove duplicate
+    ids = list(set(ids))
+    return ids
 
 
 if __name__ == "__main__":
-    work_id = "WA0RK0529"
-    related_works = get_work_metadata(work_id)
 
-    write_json(related_works, "res.json")
-    print(related_works)
+    for page_no in range(1, 45):
+        input_path = f"ཤེས་རབ་ཀྱི་ཕ་རོལ་ཏུ་ཕྱིན་པའི་སྙིང་པོ།/{page_no}.txt"
+        output_path = f"ཤེས་རབ་ཀྱི་ཕ་རོལ་ཏུ་ཕྱིན་པའི་སྙིང་པོ།_jsons/{page_no}.json"
+
+        text = Path(input_path).read_text(encoding="utf-8")
+        ids = extract_instances_id(text)
+
+        if len(ids) != 20:
+            print(f"Page no {page_no} has {len(ids)} ids.")
+
+        write_json(ids, output_path)
